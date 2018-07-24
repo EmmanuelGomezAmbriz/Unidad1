ece1dc181974eed6b4da6e95c2398881e36d17f2
15040093 <emmanuelgmz77@hotmail.com>
2018-07-24 12:26:36 -0500

    24July

--- src/Unidad_2/Practica015_JFoenix_InicioSesion/InicioSesion.java
+++ src/Unidad_2/Practica015_JFoenix_InicioSesion/InicioSesion.java
@@ -1,12 +1,17 @@
 package Unidad_2.Practica015_JFoenix_InicioSesion;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import javax.security.sasl.SaslClient;
+import java.sql.*;
 
 public class InicioSesion extends Application {
     @Override
@@ -17,5 +22,39 @@ public class InicioSesion extends Application {
         primaryStage.setScene(escena);
         primaryStage.setTitle("Inicio de Sesiòn");
         primaryStage.show();
+    }
+        @FXML
+        private JFXTextField usuario;
+
+        @FXML
+        private JFXPasswordField clave;
+
+        @FXML
+        void iniciarSesion(ActionEvent event) {
+            try {
+                Connection connection = DriverManager.getConnection("jdbc:sqlite:basedatos1.db");
+                Statement statement = connection.createStatement();
+                statement.setQueryTimeout(30);//set timeout to 30 sec.
+
+                String correo = usuario.getText();
+                String pw = clave.getText();
+
+                String consulta = "SELECT * FROM usuarios WHERE email = '"+correo+"' AND password = '"+pw+"'";
+                ResultSet rs = statement.executeQuery(consulta);
+
+                if(rs.next()){
+                    System.out.println("Usuario válido");
+                    System.out.println(rs.getString("nombre"));
+                }
+                else{
+                    System.out.println("Usuario no válido");
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Usuario: "+usuario.getText());
+            System.out.println("Clave: "+clave.getText());
+
     }
 }
